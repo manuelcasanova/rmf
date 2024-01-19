d864ed4b98ad345e459423d722e7106393f0ab83
Manuel Casanova <manucasanova@hotmail.com>
2024-01-19 04:12:48 -0800

    Add environmental variables for production cpanel whc

--- server/db.js
+++ server/db.js
@@ -9,3 +9,17 @@ const pool = new Pool({
 })
 
 module.exports = pool;
+
+// const Pool = require('pg').Pool;
+
+// const pool = new Pool({
+// user: process.env.DB_USER,
+// host: process.env.DB_HOST,
+// database: process.env.DB_DATABASE,
+// password: process.env.DB_PASSWORD,
+// port: process.env.DB_PORT
+// })
+
+
+// module.exports = pool;
+
