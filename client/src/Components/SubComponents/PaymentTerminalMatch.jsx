c944abae11054e80694a6948e969153bca2c457a
Manuel Casanova <manucasanova@hotmail.com>
2024-02-13 04:28:14 -0800

    Refactor PaymentTerminalMatch

--- client/src/Components/SubComponents/PaymentTerminalMatch.jsx
+++ client/src/Components/SubComponents/PaymentTerminalMatch.jsx
@@ -1,126 +1,67 @@
-//Hooks
-
-import { useState } from "react";
+import React, { useState } from 'react';
 
 export default function PayTerminalMatch() {
-
-
-  const [terminal, setTerminal] = useState(false)
+  const [terminal, setTerminal] = useState(false);
 
   const showTerminal = (e) => {
     e.preventDefault();
     setTerminal(prev => !prev);
+    resetValues();
+  };
+
+  const resetValues = () => {
     setVisa1(0);
     setVisa2(0);
     setMasterCard1(0);
     setMasterCard2(0);
-  }
+    setDebit1(0);
+    setDebit2(0);
+  };
 
-  const [visa1, setVisa1] = useState(0)
-  const [visa2, setVisa2] = useState(0)
-  const totalVisa = (parseFloat(visa1) + parseFloat(visa2));
+  const [visa1, setVisa1] = useState(0);
+  const [visa2, setVisa2] = useState(0);
+  const totalVisa = parseFloat(visa1) + parseFloat(visa2);
 
-  const [masterCard1, setMasterCard1] = useState(0)
-  const [masterCard2, setMasterCard2] = useState(0)
-  const totalMasterCard = (parseFloat(masterCard1) + parseFloat(masterCard2));
+  const [masterCard1, setMasterCard1] = useState(0);
+  const [masterCard2, setMasterCard2] = useState(0);
+  const totalMasterCard = parseFloat(masterCard1) + parseFloat(masterCard2);
 
-  const [debit1, setDebit1] = useState(0)
-  const [debit2, setDebit2] = useState(0)
-  const totalDebit = (parseFloat(debit1) + parseFloat(debit2));
+  const [debit1, setDebit1] = useState(0);
+  const [debit2, setDebit2] = useState(0);
+  const totalDebit = parseFloat(debit1) + parseFloat(debit2);
 
   return (
-    <div
-      className='pizza-making-question'
-    >
-
-
+    <div className='pizza-making-question'>
       <div className='yes-no'>
         {!terminal && <button className='clear-all' onClick={showTerminal}>Payment Terminal Match</button>}
       </div>
-
-
       {terminal && <button className='clear-all-x' onClick={showTerminal}>x</button>}
-      {terminal &&
-
-<>
-
-        <div className="card">
-          <div className='card1'>
-            <label className='inline-label-small'>Visa 1</label>
-            <input type="number" className='cash-input-small' value={visa1} onChange={(e) => setVisa1(e.target.value)}
-              onClick={() => setVisa1("")}
-              onFocus={() => setVisa1("")}
-            ></input>
-          </div>
-
-          <div className='card2'>
-            <label className='inline-label-small'>Visa 2</label>
-            <input type="number" className='cash-input-small' value={visa2} onChange={(e) => setVisa2(e.target.value)}
-              onClick={() => setVisa2("")}
-              onFocus={() => setVisa2("")}
-            ></input>
-          </div>
-
-          <div className='cardtotal'>
-            <label className='inline-label-small'>Total Visa:</label>
-            <label className='total-card'>{totalVisa}</label>
-          </div>
-        </div>
-
-
-        <div className="card">
-          <div className='card1'>
-            <label className='inline-label-small'>MasterCard 1</label>
-            <input type="number" className='cash-input-small' value={masterCard1} onChange={(e) => setMasterCard1(e.target.value)}
-              onClick={() => setMasterCard1("")}
-              onFocus={() => setMasterCard1("")}
-            ></input>
-          </div>
-
-          <div className='card2'>
-            <label className='inline-label-small'>MasterCard 2</label>
-            <input type="number" className='cash-input-small' value={masterCard2} onChange={(e) => setMasterCard2(e.target.value)}
-              onClick={() => setMasterCard2("")}
-              onFocus={() => setMasterCard2("")}
-            ></input>
-          </div>
-
-          <div className='cardtotal'>
-            <label className='inline-label-small'>Total MC:</label>
-            <label className='total-card'>{totalMasterCard}</label>
-          </div>
-        </div>
-
-        <div className="card">
-          <div className='card1'>
-            <label className='inline-label-small'>Debit 1</label>
-            <input type="number" className='cash-input-small' value={debit1} onChange={(e) => setDebit1(e.target.value)}
-              onClick={() => setDebit1("")}
-              onFocus={() => setDebit1("")}
-            ></input>
-          </div>
-
-          <div className='card2'>
-            <label className='inline-label-small'>Debit 2</label>
-            <input type="number" className='cash-input-small' value={debit2} onChange={(e) => setDebit2(e.target.value)}
-              onClick={() => setDebit2("")}
-              onFocus={() => setDebit2("")}
-            ></input>
-          </div>
-
-          <div className='cardtotal'>
-            <label className='inline-label-small'>Total Debit:</label>
-            <label className='total-card'>{totalDebit}</label>
-          </div>
-        </div>
-        
-        
-        </>      
-        
-        }
-
-
-
+      {terminal && (
+        <>
+          <CardInput label1='Visa 1' label2='Visa 2' value1={visa1} value2={visa2} setValue1={setVisa1} setValue2={setVisa2} total={totalVisa} totalLabel='Total Visa:' />
+          <CardInput label1='MasterCard 1' label2='MasterCard 2' value1={masterCard1} value2={masterCard2} setValue1={setMasterCard1} setValue2={setMasterCard2} total={totalMasterCard} totalLabel='Total MC:' />
+          <CardInput label1='Debit 1' label2='Debit 2' value1={debit1} value2={debit2} setValue1={setDebit1} setValue2={setDebit2} total={totalDebit} totalLabel='Total Debit:' />
+        </>
+      )}
     </div>
-  )
+  );
+}
+
+function CardInput({ label1, label2, value1, value2, setValue1, setValue2, total, totalLabel }) {
+  return (
+    <div className='card'>
+      <div className='card1'>
+        <label className='inline-label-small'>{label1}</label>
+        <input type='number' className='cash-input-small' value={value1} onChange={(e) => setValue1(e.target.value)} onClick={() => setValue1('')} onFocus={() => setValue1('')} />
+      </div>
+      <div className='card2'>
+        <label className='inline-label-small'>{label2}</label>
+        <input type='number' className='cash-input-small' value={value2} onChange={(e) => setValue2(e.target.value)} onClick={() => setValue2('')} onFocus={() => setValue2('')} />
+      </div>
+      <div className='cardtotal'>
+        <label className='inline-label-small'>{totalLabel}</label>
+        <label className='total-card'>{total}</label>
+      </div>
+    </div>
+  );
 }
