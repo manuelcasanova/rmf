6fbe3de1d67567e8ed98189831b9858206cf44ba
Manuel Casanova <manucasanova@hotmail.com>
2024-02-19 09:15:51 -0800

    Add link to developers site

--- client/src/Components/FrontPage.jsx
+++ client/src/Components/FrontPage.jsx
@@ -7,16 +7,21 @@ import { Link } from 'react-router-dom';
 const FrontPage = () => {
   return (
 
-        <div className="app-container">
-          <div className="rectangle">
-          <img src="tips-final.jpg" alt="Logo" />
-          </div>
-          <div className="shift">
-            <Link to='/am' className="am">AM</Link>
-            <Link to='/pm' className="pm">PM</Link>
-          </div>
+    <div className="app-container">
+      <div className="rectangle">
+        <img src="tips-final.jpg" alt="Logo" />
+      </div>
+      <div className="shift">
+        <Link to='/am' className="am">AM</Link>
+        <Link to='/pm' className="pm">PM</Link>
+      </div>
+      <div className="home-footer">
+        <div className="footerp">Tips! by&nbsp;
 
+        <a className="home-ext-link link-black" href="https://www.manucasanova.com/" rel="noreferrer noopener" target="_blank"> <strong>M. Casanova</strong></a>  &nbsp;
         </div>
+      </div>
+    </div>
 
   );
 };
